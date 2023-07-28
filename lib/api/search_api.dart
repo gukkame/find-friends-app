616191bbab7436bfec9a9c51fd63cfe1eb5934ca
Laura-Eliise <laura-eliis@marrandi.ee>
2023-07-28 12:41:35 +0300

    created a SearchApi and GeneralApi. Search api is responsible for all db queries related to the search view. General Api is resonsible for queries that are used in more than one view

--- lib/api/search_api.dart
+++ lib/api/search_api.dart
@@ -1,3 +1,24 @@
+import 'package:flutter/cupertino.dart';
 
+import '../utils/user.dart';
+import 'general_api.dart';
 
-// Get the list of friends, invites, and people from search bar, get data from DB
+class SearchApi extends GeneralApi {
+  /// Takes in the current user and the search input. Return
+  /// null if the friend request was sent successfully an error message otherwise.
+  Future<String?> sendFriendRequest(User user, String input) async {
+    try {
+      await update(
+        collection: "friends",
+        path: input,
+        data: {
+          "inbound": {user.email, user.name},
+        },
+      );
+      return null;
+    } catch (e) {
+      debugPrint(e.toString());
+      return "No user found";
+    }
+  }
+}
--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'general_api.dart';
 
-import 'api.dart';
-
-class UserApi extends Api {
+class UserApi extends GeneralApi {
   Future<bool> registerNewUser(
       {required String email,
       required String name,
@@ -27,16 +26,6 @@ class UserApi extends Api {
     } catch (e) {
       debugPrint(e.toString());
       return false;
-    }
-  }
-
-  Future<String?> getUsername({required String email}) async {
-    try {
-      var resp = await readPath(collection: "users", path: email);
-      debugPrint("get the doc: ${resp.data()}");
-      return (resp.data() as Map<String, dynamic>)["name"] as String;
-    } catch (e) {
-      return null;
     }
   }
 }
