882c6964fcff298f117ad0908efefbc715b544e5
gukkame <guka245@gmail.com>
2023-08-01 11:59:37 +0300

    added map and invites branch code, invites and location api

---
+++ lib/api/locations.api.dart
@@ -0,0 +1,31 @@
+import '../utils/user.dart';
+import 'general_api.dart';
+
+class LocationsApi extends GeneralApi {
+  /// Updates the position of the current user.
+  /// Returns null if the update was successful.
+  /// Returns the error message otherwise.
+  Future<String?> updatePosition(
+      {required String email, required int lat, required int lng}) async {
+    try {
+      await write(
+        collection: "locations",
+        path: email,
+        data: {
+          "lat": lat,
+          "lng": lng,
+        },
+      );
+      return null;
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  /// Sets up a listener to the database document.
+  /// When the user at the given email updates their position,
+  /// the stream will fire and give their new coordinates
+  Stream getFriendLocationUpdater(String email) {
+    return getStream(collection: "locations", path: email);
+  }
+}
