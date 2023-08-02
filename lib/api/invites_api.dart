755cd6b1b632e6ff401be5df957112044c60451b
Laura-Eliise <laura-eliis@marrandi.ee>
2023-08-02 18:08:40 +0300

    bug fix

--- lib/api/invites_api.dart
+++ lib/api/invites_api.dart
@@ -8,12 +8,12 @@ class InvitesApi extends GeneralApi {
   Future<String?> acceptInvite(User user, String email, String name) async {
     try {
       await update(collection: "friends", path: email, data: {
-        "friends": {user.email: user.name},
-        "outbound": {user.email: FieldValue.delete()}
+        "friends.${user.email}": user.name,
+        "outbound.${user.email}": FieldValue.delete()
       });
       await update(collection: "friends", path: user.email, data: {
-        "friends": {email: name},
-        "inbound": {email: FieldValue.delete()}
+        "friends.$email": name,
+        "inbound.$email": FieldValue.delete()
       });
       return null;
     } catch (e) {
@@ -23,12 +23,16 @@ class InvitesApi extends GeneralApi {
 
   Future<String?> declineInvite(User user, String email, String name) async {
     try {
-      await update(collection: "friends", path: email, data: {
-        "outbound": {user.email: FieldValue.delete()}
-      });
-      await update(collection: "friends", path: user.email, data: {
-        "inbound": {email: FieldValue.delete()}
-      });
+      await update(
+        collection: "friends",
+        path: email,
+        data: {"outbound.${user.email}": FieldValue.delete()},
+      );
+      await update(
+        collection: "friends",
+        path: user.email,
+        data: {"inbound.$email": FieldValue.delete()},
+      );
       return null;
     } catch (e) {
       return e.toString();
